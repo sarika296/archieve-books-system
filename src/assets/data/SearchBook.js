b8f4fc509bf169c7b94918347155b398a4b53d25
sarika296 <sarikadewangan94@gmail.com>
2020-05-21 02:24:46 +0530

    design of search book created

--- src/assets/data/SearchBook.js
+++ src/assets/data/SearchBook.js
@@ -4,6 +4,24 @@ class SearchBook extends Component {
     render() {
         return(
             <>
+            
+                <div className="container">
+                    <div className="card-update">
+                        <div className="card-header">
+                            <h3>Search Book</h3>
+                        </div>
+                    <form id="form">
+                    <br />
+                    <select className="drop"
+                         >
+                           <option>1</option>
+                           <option>2</option>
+                           <option>3</option>
+                         }
+                    </select><br /><br /><br />
+                    </form>
+                    </div>
+                </div>
             </>
         )
     }
